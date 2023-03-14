0fcb3c84300ecf8849c02d5103706f5b113ed61a
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-14 16:17:46 -0400

    initial commit

---
+++ src/main/java/com/myapp/AttendanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceManagementSystemApplication.class, args);
+	}
+
+}
