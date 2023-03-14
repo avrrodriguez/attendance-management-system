8e55c7c77e84957c9342b18b833477f14c1b6977
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-14 16:34:55 -0400

    Adds rest controller and get mapping in Main file

---
+++ src/main/java/com/myapp/Main.java
@@ -0,0 +1,23 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/home")
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@GetMapping
+	public String getMethod() {
+		return "Hello World";
+	}
+
+}
