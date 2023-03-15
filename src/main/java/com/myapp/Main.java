d4f377901ff3450c0872e41503958e836ad08bf1
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-14 23:06:46 -0400

    Adds rrest controller and quest mapping to main file

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/home")
+@RequestMapping("")
 public class Main {
 
 	public static void main(String[] args) {
