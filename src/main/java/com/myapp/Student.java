e970ca004d80a013ad2f18f223e579a2ebbf062d
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-15 15:31:30 -0400

    Adds missing age attribute of student class

--- src/main/java/com/myapp/Student.java
+++ src/main/java/com/myapp/Student.java
@@ -56,6 +56,14 @@ public class Student {
     return this.name;
   }
 
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Integer getAge() {
+    return this.age;
+  }
+
   public void setGender(String gender) {
     this.gender = gender;
   }
