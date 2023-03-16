999468f7ae092e95ce5fcf8cdf304409bdb759d5
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-15 21:54:30 -0400

    Adds studentrepository file as well as get classes for all students and one student by id

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -1,23 +1,41 @@
 package com.myapp;
 
+// import com.myapp.Student;
+// import com.myapp.StudentRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("")
 public class Main {
 
+	private final StudentRepository studentRepository;
+
+	public Main(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
 	@GetMapping
-	public String getMethod() {
-		return "Hello World";
+	public List<Student> getStudents(Student students) {
+		return studentRepository.findAll();
+	}
+
+	@GetMapping
+	public Optional<Student> getStudent(@PathVariable("studentId") Integer id) {
+		return studentRepository.findById(id);
 	}
 
 }
