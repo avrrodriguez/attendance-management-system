68b9dc9cdd3794885e8caa7eecdeac953c54c8b3
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-31 18:47:43 -0400

    Creates the security configuration class in security folder

---
+++ src/main/java/com/myapp/security/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package com.myapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+}
