103b0d94a6d39c23a1e4346a15f7e278cccac2d5
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-04-01 19:30:57 -0400

    Adds authentication manager and provider methods to aplication config class in security folder

--- src/main/java/com/myapp/security/ApplicationConfig.java
+++ src/main/java/com/myapp/security/ApplicationConfig.java
@@ -2,8 +2,14 @@ package com.myapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.myapp.faculty.FacultyRepository;
 
@@ -20,5 +26,23 @@ public class ApplicationConfig {
   public UserDetailsService userDetailsService() {
     return username -> repository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("user not found")); // lambda function
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+  }
+
+  private PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
